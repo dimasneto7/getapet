c8078b1e3d0687eedc5e5a6642546cb6d6f5c68e
dimasneto7 <dimas_fcn@outlook.com>
2023-11-12 14:41:31 -0300

    pet upload images

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,80 @@
+const Pet = require('../models/Pet')
+
+// helpers
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
+
+module.exports = class PetController {
+  // create a pet
+
+  static async create(req, res) {
+    const { name, age, weight, color } = req.body
+
+    const images = req.files
+
+    const available = true
+
+    // images upload
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: 'O nome é obrigatótio' })
+      return
+    }
+
+    if (!age) {
+      res.status(422).json({ message: 'A idade é obrigatótia' })
+      return
+    }
+
+    if (!weight) {
+      res.status(422).json({ message: 'O peso é obrigatótio' })
+      return
+    }
+
+    if (!color) {
+      res.status(422).json({ message: 'A cor é obrigatótia' })
+      return
+    }
+
+    if (images.length === 0) {
+      res.status(422).json({ message: 'A imagem é obrigatótia' })
+      return
+    }
+
+    // get pet owner
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    // create a pet
+    const pet = new Pet({
+      name,
+      age,
+      weight,
+      color,
+      available,
+      images: [],
+      user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    })
+
+    images.map((image) => {
+      pet.images.push(image.filename)
+    })
+
+    try {
+      const newPet = await pet.save()
+
+      res.status(201).json({
+        message: 'Pet cadastrado com sucesso!',
+        newPet,
+      })
+    } catch (error) {
+      res.status(500).json({ message: error })
+    }
+  }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const UserRoutes = require('./routes/UserRoutes')
+const PetRoutes = require('./routes/PetRoutes')
 
 const app = express()
 
@@ -13,5 +15,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.static('public'))
 
 // Routes
+app.use('/users', UserRoutes)
+app.use('/pets', PetRoutes)
 
 app.listen(5000)
