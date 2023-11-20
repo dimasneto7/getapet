8b6bde17e3fde32d27ed25bacdaf520c1f3da47c
dimasneto7 <dimas_fcn@outlook.com>
2023-11-19 23:47:32 -0300

    register user front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,39 @@
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+
+// Components
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+
+// Pages
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
+// Context
+import { UserProvider } from './context/UserContext'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Router path="/login">
+              <Login />
+            </Router>
+            <Router path="/register">
+              <Register />
+            </Router>
+            <Router path="/">
+              <Home />
+            </Router>
+          </Switch>
+        </Container>
+        <Footer />
+      </UserProvider>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+import styles from './Navbar.module.css'
+
+import Logo from '../../assets/img/logo.png'
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
+        <img src={Logo} alt="logo" />
+        <h2>Get A Pet</h2>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Adorar</Link>
+        </li>
+        <li>
+          <Link to="/login">Entrar</Link>
+        </li>
+        <li>
+          <Link to="/register">Cadastrar</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
