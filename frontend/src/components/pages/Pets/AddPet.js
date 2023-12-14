07a63b4e24c7894962cfe33ce1055c9d843f7bb9
dimasneto7 <dimas_fcn@outlook.com>
2023-12-14 20:45:53 -0300

    add pet

--- frontend/src/components/pages/Pets/AddPet.js
+++ frontend/src/components/pages/Pets/AddPet.js
@@ -11,6 +11,45 @@ import useFlashMessage from '../../../hooks/useFlashMessage'
 import PetForm from '../../form/PetForm'
 
 function AddPet() {
+  const [token] = useState(localStorage.getItem('token') || '')
+  const { setFlashMessage } = useFlashMessage()
+  const history = useHistory()
+
+  async function registerPet(pet) {
+    let msgType = 'success'
+
+    const formData = new FormData()
+
+    await Object.keys(pet).forEach((key) => {
+      if (key === 'images') {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append('images', pet[key][i])
+        }
+      } else {
+        formData.append(key, pet[key])
+      }
+    })
+
+    const data = await api
+      .post('pets/create', formData, {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multipart/form-data',
+      })
+      .then((response) => {
+        return response.data
+      })
+      .catch((err) => {
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+
+    if (msgType !== 'error') {
+      history.push(`/pets/mypets`)
+    }
+  }
+
   return (
     <section>
       <div className={styles.addpet_header}>
@@ -18,7 +57,7 @@ function AddPet() {
         <p>Depois ele ficará disponível para adoção</p>
         <p>Formulário</p>
       </div>
-      <PetForm btnText="Cadastrar Pet" />
+      <PetForm handleSubmit={registerPet} btnText="Cadastrar Pet" />
     </section>
   )
 }
