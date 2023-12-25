0e868ae461ce713f557b28975e4fad5e269a228b
dimasneto7 <dimas_fcn@outlook.com>
2023-12-25 17:37:12 -0300

    home

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -223,10 +223,7 @@ module.exports = class PetController {
       updatedData.color = color
     }
 
-    if (images.length === 0) {
-      res.status(422).json({ message: 'A imagem é obrigatótia' })
-      return
-    } else {
+    if (images.length > 0) {
       updatedData.images = []
       images.map((image) => {
         updatedData.images.push(image.filename)
